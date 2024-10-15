ee781866d03c2dec8e3cb1d213e949d70e4e5e5e
Alefesb <142458915+Alefesb@users.noreply.github.com>
2024-10-15 19:25:07 -0300

    updt

--- login.php
+++ login.php
@@ -11,7 +11,7 @@
     <input type="text" id="username" name="username" required>
 
     <label for="password">Senha:</label>
-    <input type="password" id="password" name="senha" required>
+    <input type="password" id="password" name="password" required>
 
     <button type="submit">Entrar</button>
 </form>  
@@ -21,33 +21,33 @@
 
 <?php
 require "conexao.php";
-// Conexão com o banco de dados (ajuste as informações)
+
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "password";
 $dbname = "farmacia";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificar conexão
+
 if ($conn->connect_error) {
     die("Conexão falhou: " . $conn->connect_error);
 }
 
-// Receber dados do formulário
+
 $username = $_POST['username'];
 $senha = $_POST['senha'];
 
-// Preparar e executar a consulta SQL
+
 $sql = "SELECT * FROM usuarios WHERE username = '$username' AND password = '$password'";
 $result = $conn->query($sql);
 
-// Verificar se o usuário existe
+
 if ($result->num_rows > 0) {
-    // Usuário autenticado
+    
     echo "Login bem-sucedido!";
 } else {
-    // Usuário não encontrado
+  
     echo "Usuário ou senha inválidos.";
 }
 
