939975cbfd6b58474b28440d6900f30217b75bc1
Alefesb <142458915+Alefesb@users.noreply.github.com>
2024-10-17 15:02:26 -0300

    updt

--- cadastro.php
+++ cadastro.php
@@ -6,18 +6,36 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
+<style>
+    body{
+        margin-left: 10%;
+    }
+    h1{
+        margin-left: 20%;
+    }
+</style>
 <body>
+    <h1>
+        Cadastro de Medicamentos
+    </h1>
 <form method="post" action="processa_dados.php" class="row g-3 needs-validation" novalidate>
   <div class="col-md-4">
     <label for="validationCustom01" class="form-label">Nome do medicamento</label>
-    <input name="medicamento" type="text" class="form-control" id="validationCustom01" placeholder="Nome do medicamento" required>
+    <input name="medicamento" type="text" class="form-control" id="validationCustom01" name="nome" placeholder="Nome do medicamento" required>
     <div class="valid-feedback">
       Looks good!
     </div>
   </div>
   <div class="col-md-4">
     <label for="validationCustom02" class="form-label">Preço do medicamento</label>
-    <input name="Preço" type="text" class="form-control" id="validationCustom02" placeholder="Preço" required>
+    <input name="Preço" type="text" class="form-control" id="validationCustom02" name="preço" placeholder="Preço" required>
+    <div class="valid-feedback">
+      Looks good!
+    </div>
+  </div>
+  <div class="col-md-4">
+    <label for="validationCustom02" class="form-label">Quantidade de medicamentos</label>
+    <input name="Preço" type="text" class="form-control" id="validationCustom02" name="qtd" placeholder="Preço" required>
     <div class="valid-feedback">
       Looks good!
     </div>
@@ -27,7 +45,7 @@
     <div class="input-group has-validation">
       
       
-      <select name="cat" id="" required>
+      <select name="cat" id="" name="cat" required>
         <option value="">Analgésico</option>
         <option value="">Antibiótico</option>
         <option value="">Anti-inflamatório</option>
@@ -37,49 +55,41 @@
       </div>
     </div>
   </div>
-  <div class="col-md-6">
-    <label for="validationCustom03" class="form-label">City</label>
-    <input type="text" class="form-control" id="validationCustom03" required>
-    <div class="invalid-feedback">
-      Please provide a valid city.
-    </div>
-  </div>
+  
   <div class="col-md-3">
-    <label for="validationCustom04" class="form-label">State</label>
-    <select class="form-select" id="validationCustom04" required>
-      <option selected disabled value="">Choose...</option>
-      <option>...</option>
-    </select>
+    <label for="validationCustom04" class="form-label">Data de Validade</label>
+    <input type="date" class="form-select" id="validationCustom04" name="data" required>
+      
+    <in/>
     <div class="invalid-feedback">
       Please select a valid state.
     </div>
   </div>
-  <div class="col-md-3">
-    <label for="validationCustom05" class="form-label">Zip</label>
-    <input type="text" class="form-control" id="validationCustom05" required>
-    <div class="invalid-feedback">
-      Please provide a valid zip.
-    </div>
-  </div>
+
   <div class="col-12">
     <div class="form-check">
       <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
       <label class="form-check-label" for="invalidCheck">
-        Agree to terms and conditions
+        Concorda com os termos?
       </label>
       <div class="invalid-feedback">
-        You must agree before submitting.
+       Marque a opção
       </div>
     </div>
   </div>
   <div class="col-12">
-    <button class="btn btn-primary" type="submit">Submit form</button>
+    <button class="btn btn-primary" type="submit">Cadastrar</button>
   </div>
 </form>
 
 </body>
 
 <?php 
+
+$nome = $_POST["nome"];
+$preço = $_POST["preço"];
+$categoria = $_POST["cat"];
+$data= $_POST["data"];
 
 
 ?>
--- login.php
+++ login.php
@@ -1,57 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<form action="login.php" method="post">
-    <label for="username">Nome de Usuário:</label>
-    <input type="text" id="username" name="username" required>
-
-    <label for="password">Senha:</label>
-    <input type="password" id="password" name="password" required>
-
-    <button type="submit">Entrar</button>
-</form>  
-</body>
-
-
 
 <?php
 require "conexao.php";
 
-$servername = "localhost";
-$username = "root";
-$password = "password";
-$dbname = "farmacia";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
 
 
-if ($conn->connect_error) {
-    die("Conexão falhou: " . $conn->connect_error);
+$pdo = new mysqli("localhost", "root","", "Farmacia");
+
+
+if ($pdo->connect_error) {
+    die("Conexão falhou: " . $pdo->connect_error);
 }
 
 
 $username = $_POST['username'];
-$senha = $_POST['senha'];
+$password = $_POST['password'];
 
 
-$sql = "SELECT * FROM usuarios WHERE username = '$username' AND password = '$password'";
-$result = $conn->query($sql);
+$sql = "SELECT * FROM usuario WHERE username = '$username' AND password = '$password'";
+$result = $pdo->query($sql);
 
 
 if ($result->num_rows > 0) {
     
-    echo "Login bem-sucedido!";
+    header("Location: cadastro.php");
+    exit;
 } else {
   
     echo "Usuário ou senha inválidos.";
 }
 
-$conn->close();
+$pdo->close();
 ?>
 
 </html>
