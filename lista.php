d531cb997d48bb9a76541f0948f7c590ca87b4a5
Alefesb <142458915+Alefesb@users.noreply.github.com>
2024-10-17 17:51:12 -0300

    updt

--- lista.php
+++ lista.php
@@ -3,9 +3,26 @@ require "conexao.php";
 require "telaLista.php";
 
 
-$sql = "UPDATE cadastro SET nome = :nome WHERE id = :id";
+if ($pdo->connect_error) {
+    die("Conexão falhou: " . $pdo->connect_error);
+}
 
-
-
+$sql = "SELECT * FROM cadastro";
 $result = $pdo->query($sql);
+
+if ($result->num_rows > 0) {
+    
+    while($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>" . $row["Nome"] . "</td>";
+        echo "<td>" . $row["cat"] . "</td>";
+        echo "<td>" . $row["Preço"] . "</td>";
+        // Formatar a data de validade
+        echo "<td>" . date('d/m/Y', strtotime($row["data"])) . "</td>";
+        echo "</tr>";
+    }
+} else {
+    echo "<tr><td colspan='4'>Nenhum remédio encontrado.</td></tr>";
+}
+$pdo->close();
 ?>
